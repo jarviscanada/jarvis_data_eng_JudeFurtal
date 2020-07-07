106f15ad9a91e9879bbd99b63aaa8834cbee073a
Jude Furtal <jfurtal3@gmail.com>
2020-07-07 22:33:50 +0000

    committing TwitterHttpHelper

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/service/Service.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/service/Service.java
@@ -1,3 +1,4 @@
+/*
 package ca.jrvs.apps.twitter.service;
 
 import ca.jrvs.apps.twitter.model.Tweet;
@@ -5,18 +6,21 @@ import java.util.List;
 
 public interface Service {
 
-  /**
+  */
+/**
    * Validate and post a user input Tweet
    *
    * @param tweet tweet to be created
    * @return created tweet
    *
    * @throws IllegalArgumentException if text exceed max number of allowed characters or lat/long out of range
-   */
+   *//*
+
   Tweet postTweet(Tweet tweet);
 
 
-  /**
+  */
+/**
    * Search a tweet by ID
    *
    * @param id tweet id
@@ -24,17 +28,20 @@ public interface Service {
    * @return Tweet object which is returned by the Twitter API
    *
    * @throws IllegalArgumentException if id or fields param is invalid
-   */
+   *//*
+
   Tweet showTweet(String id, String[] fields);
 
-  /**
+  */
+/**
    * Delete Tweet(s) by id(s).
    *
    * @param ids tweet IDs which will be deleted
    * @return A list of Tweets
    *
    * @throws IllegalArgumentException if one of the IDs is invalid.
-   */
+   *//*
+
   List<Tweet> deleteTweets(String[] ids);
 
-}
+}*/
