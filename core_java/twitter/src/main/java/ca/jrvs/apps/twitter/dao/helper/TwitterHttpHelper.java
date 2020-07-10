9d45fd841449ae3c179b43550be53734096532de
Jude Furtal <jfurtal3@gmail.com>
2020-07-10 20:50:32 +0000

    committing changes

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/helper/TwitterHttpHelper.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/helper/TwitterHttpHelper.java
@@ -21,7 +21,6 @@ public class TwitterHttpHelper implements HttpHelper {
 
   private final Logger logger = LoggerFactory.getLogger(TwitterHttpHelper.class);
 
-
   /**
    * Dependencies are specified as private member variables
    */
@@ -49,23 +48,6 @@ public class TwitterHttpHelper implements HttpHelper {
      */
 
     httpClient = new DefaultHttpClient();
-
-  }
-
-  public static void main(String[] args) throws Exception {
-
-    String consumerKey = System.getenv("consumerKey");
-    String consumerSecret = System.getenv("consumerSecret");
-    String accessToken = System.getenv("accessToken");
-    String tokenSecret = System.getenv("tokenSecret");
-
-    System.out.println(consumerKey + "|" + consumerSecret + "|" + accessToken + "|" + tokenSecret);
-
-    //Create components
-    /*HttpHelper httpHelper = new TwitterHttpHelper(consumerKey, consumerSecret, accessToken, tokenSecret);
-
-    HttpResponse response = httpHelper.httpPost(new URI("https://api.twitter.com/1.1/statuses/update.json?status=tweet_test"));
-    System.out.println(EntityUtils.toString(response.getEntity()));*/
 
   }
 
